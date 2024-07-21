184697673466688a0b75189e70a36c3c11ccb314
D1m1tr1 <d1m1tr1.d3l3tang@gmail.com>
2024-07-21 23:17:01 +0200

    fix sidebar menu item compact visual effect

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import { NavLink } from "react-router-dom";
+import { useSidebarContext } from "../context/useSidebarContext";
 
 type MenuItemProps = {
     text: string;
@@ -8,19 +9,27 @@ type MenuItemProps = {
 };
 
 const MenuItem: FC<MenuItemProps> = ({ href, icon, text }) => {
+    const {expand} = useSidebarContext();
+
     return (
-        <li className="text-nowrap overflow-hidden my-1">
+        <li className="text-nowrap my-1">
             <NavLink
                 to={href ? href : "#"}
                 className={({ isActive }) => {
                     return [
-                        `flex items-center rounded-lg p-3 text-rio-grande-900 hover:bg-rio-grande-300 w-full`,
+                        `relative flex items-center rounded-lg p-3 text-rio-grande-900 hover:bg-rio-grande-300`,
+                        `group`,
                         isActive ? "bg-rio-grande-300" : ""
                     ].join(" ")
                 }}
             >
                 {icon && <span className="">{icon}</span>}
-                <span className="ml-3 overflow-hidden">{text}</span>
+                <span className={`
+                    ml-3
+                    ${expand ? "" : "opacity-0 group-hover:absolute group-hover:transition-opacity group-hover:opacity-100 group-hover:block group-hover:left-full group-hover:bg-rio-grande-300 group-hover:py-1 group-hover:px-2 group-hover:shadow-md rounded-md"}
+                `}>
+                    {text}
+                </span>
             </NavLink>
         </li>
     );
